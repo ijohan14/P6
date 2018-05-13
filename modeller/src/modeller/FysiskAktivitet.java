8e01721a2a22288524ac75e7de2aee2a332c4b38
ijohan14 <ijohan14@student.aau.dk>
2018-05-13 22:14:42 +0200

    Stopuret til fysisk aktivitet virker. Den tæller sekunder en smule for langsomt, men ved virkelig ikke hvorfor. Ellers virker den fint, både start, pause og stop knappe virker :)

--- modeller/src/modeller/FysiskAktivitet.java
+++ modeller/src/modeller/FysiskAktivitet.java
@@ -1,32 +1,113 @@
 package modeller;
 
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-public class FysiskAktivitet{
+public class FysiskAktivitet {
 
-    @FXML
-    private Text tTimer;
-
-    @FXML
-    private Text mTimer;
-
-    @FXML
-    private Text sTimer;
-
-    @FXML
-    private Button startTimerKnap;
-
-    @FXML
-    private Button stopTimerKnap;
     
+    @FXML
+    private Text timerDisplay;
+
+    @FXML
+    private Text minutterDisplay;
+
+    @FXML
+    private Text sekunderDisplay;
+
+    @FXML
+    private Button startKnap;
+
+    @FXML
+    private Button stopKnap;
+
+    @FXML
+    private Button pauseKnap;
+
+    static int milisekunder =0;
     static int sekunder = 0;
     static int minutter = 0;
     static int timer = 0;
-    static boolean stadie = true;
+    static boolean stadie = false;
+      
     
     
+    @FXML
+    private void handleStartKnap() throws IOException{
+        
+        
+        stadie = true;
+        
+        Thread stopUr = new Thread(){
+            public void run(){
+                for(;;){
+                    if(stadie == true){
+                        try{
+                            sleep(1);
+                            if(milisekunder > 1000){
+                                milisekunder = 0;
+                                sekunder ++;
+                            }
+                            if(sekunder > 60){
+                                milisekunder = 0;
+                                sekunder = 0;
+                                minutter ++;
+                            }
+                            if(minutter > 60){
+                                milisekunder = 0;
+                                sekunder = 0;
+                                minutter = 0;
+                                timer ++;
+                            }
+                            milisekunder ++;
+                            sekunderDisplay.setText(" : "+sekunder);
+                            
+                            minutterDisplay.setText(" : "+minutter);
+                            timerDisplay.setText(" "+timer);
+                        }
+                        catch(Exception e){
+                        }
+                    }
+                    else{
+                        break;
+                    }
+                }
+            }
+        };
+        stopUr.start();
+        
+    }
     
-
+    @FXML
+    private void handlePauseKnap() throws IOException{
+        stadie = false;
+    }
+    
+    @FXML
+    private void handleStopKnap() throws IOException{
+        stadie = false;
+        timer = 0;
+        minutter = 0;
+        sekunder = 0;
+        milisekunder = 0;
+        
+        timerDisplay.setText("00 : ");
+        minutterDisplay.setText("00 : ");
+        sekunderDisplay.setText("00");
+        
+    }
 }
+
